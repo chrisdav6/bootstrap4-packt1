06f86712633e9bdebad090f927b27e9e6f470106
Chris Davis <chris.davis5440@gmail.com>
2018-03-15 14:28:49 +0000

    Completed modal and tooltips

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,10 @@
 $(function() {
   
-  //Handle Tweet Form
+  //Enable Bootstrap tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+  
+  
+  //Handle Whats happening Tweet Form
   $("#whatsHappening form").on("submit", function(e) {
     e.preventDefault();
     $("#whatsHappening").hide();
@@ -17,6 +21,42 @@ $(function() {
   $('#madeTweet button').on('click', "span", function () {
     $("#madeTweet").hide();
     $("#whatsHappening").show();
+  });
+  
+  
+  //Handle Modal
+  $(".modal-tweet-form").on("submit", function(e) {
+    e.preventDefault();
+    $("#tweet-modal").modal("hide");
+    $("#tweetText").val("");
+    $(".char-count").text("280");
+    $("#whatsHappening").hide();
+    $("#progress").fadeIn();
+    for(var i = 1; i <= 100; i++) {
+      $(".progress-bar").css("width", i + "%");
+    }
+    $("#progress").fadeOut(function() {
+      $(".progress-bar").css("width", "0%");
+      $("#madeTweet").show();
+    });
+  });
+  
+  $(".modal-close-tweet").on("click", function() {
+    $("#tweetText").val("");
+    $(".char-count").text("280");
+  });
+  
+  
+  //Handle Modal Character count
+  $("#tweetText").on("keyup", function() {
+    var maxLength = $(this).attr("maxlength");
+    var currentLength = $(this).val().length;
+    
+    if(currentLength >= maxLength) {
+      $(".char-count").text("You have reached your character limit!");
+    } else {
+      $(".char-count").text(maxLength - currentLength + " characters left");
+    }
   });
   
 });
